5ff713962d495193626ea22d19317d51ffe46f18
eunkr82 <eun.kr82@gmail.com>
2025-12-31 06:01:39 +0900

    init: ESLint, Prettier 초기 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,43 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default defineConfig(
+  {
+    ignores: [
+      'dist',
+      'build',
+      'out',
+      'coverage',
+      'node_modules',
+      '.pnpm-store',
+    ],
+  },
+
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+
+  {
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      ...reactRefresh.configs.vite.rules,
+    },
+  },
+
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
   },
-])
+  prettierConfig,
+);
