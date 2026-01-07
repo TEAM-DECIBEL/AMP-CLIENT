eda7cfd2ad62c2772ae8864ace00004360f1b343
임지수 <dlawltn0708@naver.com>
2026-01-08 01:23:52 +0900

    chore: js 추적 제거 및 gitignore 추가

--- apps/host/src/pages/home/ui/Home.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-const Home = () => {
-    return _jsx("div", { children: "\uD648\uD654\uBA74" });
-};
-export default Home;
--- apps/onboarding/src/pages/login/ui/Login.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-const Login = () => {
-    return _jsx("div", { children: "\uB85C\uADF8\uC778 \uD398\uC774\uC9C0" });
-};
-export default Login;
