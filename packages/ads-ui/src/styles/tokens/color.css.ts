950a9e0ca303a2541beaec7c4097f844ccd04cb2
임지수 <dlawltn0708@naver.com>
2026-01-08 02:21:15 +0900

    feat: 컬러 토큰 추가

---
+++ packages/ads-ui/src/styles/tokens/color.css.ts
@@ -0,0 +1,26 @@
+export const color = {
+  primary: {
+    amp_primary: "#36D69D",
+    amp_primary_deep: "#16C284",
+    amp_primary_light: "#D5FFED",
+    amp_primary_light2: "#EDFBF5",
+    amp_primary_light3: "#A6E6CA",
+  },
+  sub: {
+    amp_sub_1: "#1D1D28",
+  },
+  grayscale: {
+    amp_000: "#FFFFFF",
+    amp_50: "#F8FAFB",
+    amp_100: "#F2F3F7",
+    amp_200: "#E2E6EE",
+    amp_400: "#A9B0BB",
+    amp_500: "#838992",
+    amp_700: "#565B65",
+    amp_800: "#3E4249",
+    amp_900: "#1D2028",
+    amp_999: "#000000",
+    amp_999_50: "rgba(0, 0, 0, 0.5)",
+    amp_999_20: "rgba(0, 0, 0, 0.2)",
+  },
+};
