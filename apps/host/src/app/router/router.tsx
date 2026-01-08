86d7870ac2405ecf3cc87e57fc80c6b7b9f83d58
eunkr82 <eun.kr82@gmail.com>
2026-01-09 00:58:32 +0900

    refactor: route-level lazy 적용

--- apps/host/src/app/router/router.tsx
+++ apps/host/src/app/router/router.tsx
@@ -1,33 +1,6 @@
 import { ROUTE_PATH } from "@shared/constants/path";
 import { createBrowserRouter } from "react-router";
-import { lazy } from "react";
 import { Layout } from "./layout";
-
-const HomePage = lazy(() =>
-  import("@pages/home").then((res) => ({
-    default: res.HomePage,
-  }))
-);
-
-const EventCreatePage = lazy(() =>
-  import("@pages/event-create").then((res) => ({
-    default: res.EventCreatePage,
-  }))
-);
-
-const NoticeListPage = lazy(() =>
-  import("@pages/notice-list").then((res) => ({ default: res.NoticeListPage }))
-);
-
-const NoticeCreatePage = lazy(() =>
-  import("@pages/notice-create").then((res) => ({
-    default: res.NoticeCreatePage,
-  }))
-);
-
-const MyPage = lazy(() =>
-  import("@pages/mypage").then((res) => ({ default: res.MyPage }))
-);
 
 export const router = createBrowserRouter([
   {
@@ -35,24 +8,39 @@ export const router = createBrowserRouter([
     element: <Layout />,
     children: [
       {
-        path: ROUTE_PATH.HOST_HOME,
-        element: <HomePage />,
+        index: true,
+        lazy: async () => {
+          const mod = await import("@pages/home");
+          return { Component: mod.HomePage };
+        },
       },
       {
         path: ROUTE_PATH.EVENT_CREATE,
-        element: <EventCreatePage />,
+        lazy: async () => {
+          const mod = await import("@pages/event-create");
+          return { Component: mod.EventCreatePage };
+        },
       },
       {
         path: ROUTE_PATH.NOTICE_LIST,
-        element: <NoticeListPage />,
+        lazy: async () => {
+          const mod = await import("@pages/notice-list");
+          return { Component: mod.NoticeListPage };
+        },
       },
       {
         path: ROUTE_PATH.NOTICE_CREATE,
-        element: <NoticeCreatePage />,
+        lazy: async () => {
+          const mod = await import("@pages/notice-create");
+          return { Component: mod.NoticeCreatePage };
+        },
       },
       {
         path: ROUTE_PATH.MYPAGE,
-        element: <MyPage />,
+        lazy: async () => {
+          const mod = await import("@pages/mypage");
+          return { Component: mod.MyPage };
+        },
       },
     ],
   },
--- apps/host/src/shared/constants/path.ts
+++ apps/host/src/shared/constants/path.ts
@@ -1,5 +1,4 @@
 export const ROUTE_PATH = {
-  HOST_HOME: "/",
   EVENT_CREATE: "/events/new",
   NOTICE_LIST: "/events/:eventId/notices",
   NOTICE_CREATE: "/events/:eventId/notices/new",
