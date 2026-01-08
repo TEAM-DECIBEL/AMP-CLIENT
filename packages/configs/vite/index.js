a201f3aaa22ae10de335751fde4a1b25e20dc9ec
JS MacBook Pro <j_seokpark@naver.com>
2026-01-08 17:15:50 +0900

    fix: 빌드 에러 해결

--- packages/configs/vite/index.js
+++ packages/configs/vite/index.js
@@ -1,12 +1,15 @@
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "node:path";
 
 /**
  * @param {import('vite').UserConfig} overrides
  * @returns {import('vite').UserConfig}
  */
 export function baseViteConfig(overrides = {}) {
+  const rootDir = overrides.root ? path.resolve(overrides.root) : process.cwd();
+
   return {
     ...overrides,
     plugins: [
@@ -16,7 +19,9 @@ export function baseViteConfig(overrides = {}) {
         },
       }),
       svgr(),
-      tsconfigPaths(),
+      tsconfigPaths({
+        projects: [path.join(rootDir, "tsconfig.json")],
+      }),
       ...(overrides.plugins ?? []),
     ],
   };
