0c87739517fa344a913df6de1004535e6af25c7c
임지수 <dlawltn0708@naver.com>
2026-01-11 03:16:45 +0900

    refactor: import 경로 정리

--- packages/ads-ui/src/components/tabs/tabs.css.ts
+++ packages/ads-ui/src/components/tabs/tabs.css.ts
@@ -1,7 +1,7 @@
 import { style } from '@vanilla-extract/css';
 import { recipe } from '@vanilla-extract/recipes';
 
-import { ampThemeVars } from '../../styles/theme.css';
+import { ampThemeVars } from '../../styles';
 
 export const root = style({
   display: 'block',
