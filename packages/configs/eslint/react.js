bc39b0180012c0114cdc84102fce5cf7dc1a559d
JS MacBook Pro <j_seokpark@naver.com>
2026-01-10 14:20:50 +0900

    chore: react 린트 설정에 base 설정 상속

--- packages/configs/eslint/react.js
+++ packages/configs/eslint/react.js
@@ -1,11 +1,14 @@
-import pluginReact from 'eslint-plugin-react';
-import pluginReactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import simpleImportSort from 'eslint-plugin-simple-import-sort';
-import globals from 'globals';
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
+
+import baseConfig from "./base.js";
 
 /** @type {import("eslint").Linter.FlatConfig[]} */
 export const config = [
+  ...baseConfig,
   pluginReact.configs.flat.recommended,
 
   {
@@ -19,33 +22,33 @@ export const config = [
 
   {
     plugins: {
-      'react-hooks': pluginReactHooks,
-      'react-refresh': reactRefresh,
-      'simple-import-sort': simpleImportSort,
+      "react-hooks": pluginReactHooks,
+      "react-refresh": reactRefresh,
+      "simple-import-sort": simpleImportSort,
     },
     settings: {
-      react: { version: 'detect' },
+      react: { version: "detect" },
     },
     rules: {
       ...pluginReactHooks.configs.recommended.rules,
-      'react/react-in-jsx-scope': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
+      "react/react-in-jsx-scope": "off",
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
 
-      'simple-import-sort/imports': [
-        'error',
+      "simple-import-sort/imports": [
+        "error",
         {
           groups: [
-            ['^react$', '^@?\\w'],
-            ['^\\u0000'],
-            ['^\\.'],
-            ['^.+\\.css$'],
+            ["^react$", "^@?\\w"],
+            ["^\\u0000"],
+            ["^\\."],
+            ["^.+\\.css$"],
           ],
         },
       ],
-      'simple-import-sort/exports': 'error',
+      "simple-import-sort/exports": "error",
     },
   },
 ];
