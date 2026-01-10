5686b7bf899b5544da3ce164a4f8932f339abe4a
Naeun Lim <122524310+eunkr82@users.noreply.github.com>
2026-01-10 14:10:01 +0900

    [Init] 라우터 초기세팅 (#31)
    * chore: react-router 설치
    
    * feat: host 라우팅 설정
    
    * feat: lazy loading 적용
    
    * refactor: route-level lazy 적용
    
    * feat: onboarding 라우터 세팅
    
    * feat: 공지 상세 페이지 추가
    
    * feat: 관객 라우팅 세팅
    
    * fix: 리뷰사항 반영
    
    * chore: rect-router 패키지 임포트 경로 수정
    
    * rename: 파일명을 폴더명과 통일
    
    * remove: index.ts 파일 삭제
    
    * chore: react-router를  catalog:로 관리 통일
    
    * refactor: lazy loading, 라우트 정의 분리
    
    * chore: 불필요 구문 삭제
    
    * fix:  CI 오류 수정
    
    * chore: 오타 수정
    
    * fix: lazy-loaded 컴포넌트 정상 import
    
    * fix: CI 통과를 위해 pnpm-lock.yaml 파일 재생성

---
+++ apps/audience/src/app/router/router.tsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router";
+import { Layout } from "./layout";
+import { globalRoutes } from "./routes";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [...globalRoutes],
+  },
+]);
