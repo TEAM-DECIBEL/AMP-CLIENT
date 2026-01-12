bba3fabd3dbbb251b5d5389e95f7b3ce47322c61
임지수 <dlawltn0708@naver.com>
2026-01-12 09:06:51 +0900

    feat: 임시 로고 svg 추가

--- packages/ads-ui/src/icons/index.ts
+++ packages/ads-ui/src/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as AlertIcon } from './svgs/icn_alert.svg?react';
 export { default as BackIcon } from './svgs/icn_back.svg?react';
+export { default as ExLogoIcon } from './svgs/icn_exlogo.svg?react';
 export { default as MyPageIcon } from './svgs/icn_mypage.svg?react';
