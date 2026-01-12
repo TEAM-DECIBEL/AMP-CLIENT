6b4bb29bd3c3f15158fdae815e585e6d3eb59a4b
eunkr82 <eun.kr82@gmail.com>
2026-01-12 20:35:18 +0900

    rename: icn_btn을 icn_arrow로 이름 변경

--- packages/ads-ui/src/icons/index.ts
+++ packages/ads-ui/src/icons/index.ts
@@ -1,6 +1,6 @@
 export { default as AmpFlagIcon } from './svgs/icn_ampflag.svg?react';
+export { default as ArrowIcon } from './svgs/icn_arrow.svg?react';
 export { default as BackIcon } from './svgs/icn_back.svg?react';
-export { default as BtnIcon } from './svgs/icn_btn.svg?react';
 export { default as CalendarIcon } from './svgs/icn_calendar.svg?react';
 export { default as CameraIcon } from './svgs/icn_camera.svg?react';
 export { default as ChatIcon } from './svgs/icn_chat.svg?react';
