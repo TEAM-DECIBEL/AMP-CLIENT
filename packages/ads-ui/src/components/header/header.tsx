dcc01f8d6994c9a66d653944006ae6dbdea66caa
임지수 <dlawltn0708@naver.com>
2026-01-12 09:33:29 +0900

    feat: 헤더 기본 구조 추가

--- packages/ads-ui/src/components/header/header.tsx
+++ packages/ads-ui/src/components/header/header.tsx
@@ -1,6 +1,48 @@
-const Header = () => {
+import { AlertIcon, BackIcon, ExLogoIcon, MyPageIcon } from '../../icons';
+
+interface HeaderProps {
+  variant: 'host' | 'audience';
+  kind: 'main' | 'sub';
+  title?: string;
+}
+
+const Header = ({
+  variant,
+  kind,
+  title,
+}: HeaderProps) => {
+  const isMain = kind === 'main';
+  const isSub = kind === 'sub';
+  const showAlert = isMain && variant === 'audience';
+  const showMyPage = isMain;
+
   return (
     <header>
+      {isMain && (
+        <div>
+          <ExLogoIcon />
+          <div>
+            {showAlert && (
+              <button type="button">
+                <AlertIcon />
+              </button>
+            )}
+            {showMyPage && (
+              <button type="button">
+                <MyPageIcon />
+              </button>
+            )}
+          </div>
+        </div>
+      )}
+      {isSub && (
+        <div>
+          <button type="button">
+            <BackIcon />
+          </button>
+          {title && <h1>{title}</h1>}
+        </div>
+      )}
     </header>
   );
 };
