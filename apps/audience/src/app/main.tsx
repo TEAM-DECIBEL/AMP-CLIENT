7cba6872906982e9b27f0758d7ae4955a274b465
임지수 <dlawltn0708@naver.com>
2026-01-12 23:51:51 +0900

    feat: 오버레이 위치 설정

--- apps/audience/src/app/main.tsx
+++ apps/audience/src/app/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
 import { OverlayProvider } from 'overlay-kit';
+import { createRoot } from 'react-dom/client';
 
 import App from '@app/App';
 
