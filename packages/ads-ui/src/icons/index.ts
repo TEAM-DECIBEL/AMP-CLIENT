c381752940c35c6448ad8c00834b3be92ce73d84
임지수 <dlawltn0708@naver.com>
2026-01-12 09:00:55 +0900

    feat: 헤더 아이콘 추가

---
+++ packages/ads-ui/src/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as AlertIcon } from './svgs/icn_alert.svg?react';
+export { default as BackIcon } from './svgs/icn_back.svg?react';
+export { default as MyPageIcon } from './svgs/icn_mypage.svg?react';
