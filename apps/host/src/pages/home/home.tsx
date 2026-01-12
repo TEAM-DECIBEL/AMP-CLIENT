4c20198370aacc20b0831e587c353c714c808463
임지수 <dlawltn0708@naver.com>
2026-01-13 01:12:43 +0900

    refactor: 닫기 로직 분리

--- apps/host/src/pages/home/home.tsx
+++ apps/host/src/pages/home/home.tsx
@@ -1,5 +1,36 @@
+import { overlay } from 'overlay-kit';
+
+import { Modal, RectButton } from '@amp/ads-ui';
+
 const HomePage = () => {
-  return <div> 호스트 홈 페이지</div>;
+  const handleOpenModal = () => {
+    overlay.open(({ isOpen, close, unmount }) => {
+      const handleClose = () => {
+        close();
+        unmount();
+      };
+
+      return (
+        <Modal
+          open={isOpen}
+          title='공지를 삭제하시겠어요?'
+          description='삭제하면 복구할 수 없어요.'
+          cancelText='취소'
+          confirmText='삭제'
+          onCancel={handleClose}
+          onConfirm={handleClose}
+        />
+      );
+    });
+  };
+
+  return (
+    <div>
+      <RectButton variant='primary' onClick={handleOpenModal}>
+        모달
+      </RectButton>
+    </div>
+  );
 };
 
 export default HomePage;
