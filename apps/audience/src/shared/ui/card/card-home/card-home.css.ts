99cdb0e7329cdc646e395eb3511773f2107eb445
JS MacBook Pro <j_seokpark@naver.com>
2026-01-14 03:41:55 +0900

    feat: 홈 카드 컴포넌트 구현

---
+++ apps/audience/src/shared/ui/card/card-home/card-home.css.ts
@@ -0,0 +1,71 @@
+import { style } from '@vanilla-extract/css';
+
+import { ampThemeVars } from '@amp/ads-ui/styles';
+
+export const background = style({
+  width: '30.9rem',
+  height: '10.7rem',
+  padding: '1.2rem 1.5rem',
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+});
+
+export const contentContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '2rem',
+  paddingLeft: '0.1rem',
+});
+
+export const titleSection = style({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '0.4rem',
+  paddingRight: '0.5rem',
+  color: ampThemeVars.color.gray_900,
+  ...ampThemeVars.font.title_sb_18,
+});
+
+export const descriptionSection = style({
+  display: 'flex',
+  justifyContent: 'space-between',
+});
+
+export const description = style({
+  display: 'flex',
+  flexDirection: 'column',
+  color: ampThemeVars.color.gray_500,
+});
+
+export const location = style({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '0.4rem',
+});
+
+export const dday = style({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '0.4rem',
+  color: ampThemeVars.color.primary,
+  ...ampThemeVars.font.heading_b_22,
+});
+
+export const ddayText = style({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: '0.35rem 1rem',
+  backgroundColor: ampThemeVars.color.primary_light2,
+  borderRadius: '6px',
+});
+
+export const wifiIcon = style({
+  width: '2.2rem',
+  height: '2.2rem',
+});
+
+export const locateIcon = style({
+  width: '1.4rem',
+  height: '1.4rem',
+});
---
+++ apps/audience/src/shared/ui/card/card-home/card-home.tsx
@@ -0,0 +1,56 @@
+import { WifiIcon } from '@amp/ads-ui/icons';
+import { LocateIcon } from '@amp/ads-ui/icons';
+
+import fixedBgImage from '@shared/assets/home/card_home.webp';
+
+import * as styles from './card-home.css';
+
+interface CardHomeProps {
+  title: string;
+  location: string;
+  startDate: string;
+  endDate: string;
+  dday: number;
+}
+
+const CardHome = ({
+  title,
+  location,
+  startDate,
+  endDate,
+  dday,
+}: CardHomeProps) => {
+  return (
+    <article
+      className={styles.background}
+      style={{ backgroundImage: `url(${fixedBgImage})` }}
+      role='img'
+      aria-label='CardHome Cover Image'
+    >
+      <div className={styles.contentContainer}>
+        <div className={styles.titleSection}>
+          <WifiIcon className={styles.wifiIcon} />
+          <p>{title}</p>
+        </div>
+        <div className={styles.descriptionSection}>
+          <div className={styles.description}>
+            <div className={styles.location}>
+              <LocateIcon className={styles.locateIcon} />
+              <p>{location} 일대</p>
+            </div>
+            <p>
+              {startDate} - {endDate}
+            </p>
+          </div>
+          <div className={styles.dday}>
+            <p className={styles.ddayText}>D</p>
+            <span>-</span>
+            <p className={styles.ddayText}>{dday === 0 ? 'Day' : dday}</p>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default CardHome;
