c0c936590d6f5775cdea0a2cbe4dd6123ecc8a17
임지수 <dlawltn0708@naver.com>
2026-01-14 00:52:09 +0900

    chore: 폴더 구조 정리

--- packages/ads-ui/src/components/index.ts
+++ packages/ads-ui/src/components/index.ts
@@ -1,8 +1,8 @@
 export { default as CircleButton } from './button/circle-button/circle-button';
 export { default as CtaButton } from './button/cta-button/cta-button';
 export { default as FlagButton } from './button/flag-button/flag-button';
-export { default as RectButton } from './button/rect-button/rect-button';
 export { default as Chip } from './chip/chip';
 export { default as Header } from './header/header';
 export { default as Modal } from './modal/modal';
+export { default as RectButton } from './modal/rect-button/rect-button';
 export { default as Tabs } from './tabs/tabs';
