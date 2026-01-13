6c61c333018b5f794338323e7831a46dc562b45c
eunkr82 <eun.kr82@gmail.com>
2026-01-13 15:02:33 +0900

    feat: AddToWatchButton 컴포넌트 구현

--- packages/ads-ui/src/components/index.ts
+++ packages/ads-ui/src/components/index.ts
@@ -1,6 +1,6 @@
+export { default as AddToWatchButton } from './button/add-to-watch/add-to-watch';
 export { default as CircleButton } from './button/circle-button/circle-button';
 export { default as CtaButton } from './button/cta-button/cta-button';
 export { default as Chip } from './chip/chip';
 export { default as Header } from './header/header';
 export { default as Tabs } from './tabs/tabs';
-
