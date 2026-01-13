543a3b30cfcdc033535568501580ec873497cf44
임지수 <dlawltn0708@naver.com>
2026-01-14 01:19:25 +0900

    chore: 불필요한 코드 삭제

--- packages/ads-ui/src/components/index.ts
+++ packages/ads-ui/src/components/index.ts
@@ -1,6 +1,4 @@
 export { default as BottomSheet } from './bottom-sheet/bottom-sheet';
-export { default as ActionSheet } from './bottom-sheet/variants/action-sheet/action-sheet';
-export { default as OptionSheet } from './bottom-sheet/variants/option-sheet/option-sheet';
 export { default as CircleButton } from './button/circle-button/circle-button';
 export { default as CtaButton } from './button/cta-button/cta-button';
 export { default as FlagButton } from './button/flag-button/flag-button';
