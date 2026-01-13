e25e6b05f6b01e4a3a29049a014b39e66ea13e32
임지수 <dlawltn0708@naver.com>
2026-01-13 17:41:51 +0900

    feat: 서브 레이아웃 추가

--- apps/host/src/app/router/sub-layout.tsx
+++ apps/host/src/app/router/sub-layout.tsx
@@ -3,10 +3,10 @@ import { Outlet } from 'react-router';
 
 import { Header } from '@amp/ads-ui';
 
-export const BasicLayout = () => {
+export const SubLayout = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <Header variant='host' kind='main' />
+      <Header variant='host' kind='sub' />
       <Outlet />
     </Suspense>
   );
