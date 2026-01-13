c7cb3ffe74bfee93041f9f346dd06bca19a5194b
eunkr82 <eun.kr82@gmail.com>
2026-01-13 13:54:32 +0900

    chore: 변경된 파일명 index.ts에 적용

--- packages/ads-ui/src/components/index.ts
+++ packages/ads-ui/src/components/index.ts
@@ -1,5 +1,5 @@
 export { default as CircleButton } from './button/circle-button/circle-button';
 export { default as CtaButton } from './button/cta-button/cta-button';
+export { default as CategoryButton } from './category-button/category-button';
 export { default as Chip } from './chip/chip';
-export { default as ChipButton } from './chip-button/chip-button';
 export { default as Tabs } from './tabs/tabs';
