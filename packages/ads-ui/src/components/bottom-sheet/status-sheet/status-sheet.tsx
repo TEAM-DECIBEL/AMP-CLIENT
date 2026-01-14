0c8ee553614afbbffd18adcca268d5a80f270dc2
임지수 <dlawltn0708@naver.com>
2026-01-14 17:22:31 +0900

    refactor: 상수 파일 분리

--- packages/ads-ui/src/components/bottom-sheet/status-sheet/status-sheet.tsx
+++ packages/ads-ui/src/components/bottom-sheet/status-sheet/status-sheet.tsx
@@ -3,13 +3,14 @@ import type { ReactNode } from 'react';
 import { NoticeIcon } from '../../../icons';
 import CtaButton from '../../button/cta-button/cta-button';
 import BottomSheet from '../bottom-sheet';
+import {
+  EMPTY_DESCRIPTION,
+  EMPTY_TITLE,
+  STATUS_DESCRIPTION,
+  type StatusSheetValue,
+} from './status-sheet';
 
 import * as styles from './status-sheet.css';
-
-export type StatusSheetValue = '여유' | '보통' | '혼잡';
-const STATUS_DESCRIPTION = '현재 상황에 가장 가까운 상태를 선택해 주세요.';
-const EMPTY_TITLE = '아직 현장 상황 입력 시간이 아니에요!';
-const EMPTY_DESCRIPTION = '공연 시작 시간 8시간 전부터 입력이 가능해요.';
 
 interface StatusSheetBaseProps {
   open: boolean;
@@ -30,11 +31,7 @@ interface StatusSheetReadonlyProps extends StatusSheetBaseProps {
 
 type StatusSheetProps = StatusSheetSelectableProps | StatusSheetReadonlyProps;
 
-const StatusSelectableContent = ({
-  title,
-}: {
-  title: ReactNode;
-}) => {
+const StatusSelectableContent = ({ title }: { title: ReactNode }) => {
   return (
     <>
       <BottomSheet.Header>
