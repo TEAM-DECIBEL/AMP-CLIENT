f6a5ff35e89c6751297aa930c9be01ca76ce48e8
JS MacBook Pro <j_seokpark@naver.com>
2026-01-15 04:18:00 +0900

    refactor: 코드 리뷰 반영

--- apps/audience/src/widgets/banner/home-banner/home-banner.tsx
+++ apps/audience/src/widgets/banner/home-banner/home-banner.tsx
@@ -22,28 +22,38 @@ type HomeBannerProps = HomeBannerCardProps | HomeBannerNoneProps;
 
 const HomeBanner = (props: HomeBannerProps) => {
   const { nickname, status } = props;
+
+  const message =
+    status === 'card' ? (
+      <>
+        다가오는 <br /> 공연을 확인해보세요!
+      </>
+    ) : (
+      <>
+        일정이 아직 없어요 <br /> 공연을 추가해보세요!
+      </>
+    );
+
+  if (status === 'card') {
+    const { title, location, date, dday } = props;
+
+    return (
+      <article className={styles.banner}>
+        <p className={styles.text}>
+          <span className={styles.nickname}>{nickname}님, </span>
+          {message}
+        </p>
+        <CardHome title={title} location={location} date={date} dday={dday} />
+      </article>
+    );
+  }
+
   return (
     <article className={styles.banner}>
       <p className={styles.text}>
-        <span className={styles.nickname}>{`${nickname}님, `}</span>
-        {status === 'card' ? (
-          <>
-            다가오는 <br /> 공연을 확인해보세요!
-          </>
-        ) : (
-          <>
-            일정이 아직 없어요 <br /> 공연을 추가해보세요!
-          </>
-        )}
+        <span className={styles.nickname}>{nickname}님, </span>
+        {message}
       </p>
-      {status === 'card' && (
-        <CardHome
-          title={props.title}
-          location={props.location}
-          date={props.date}
-          dday={props.dday}
-        />
-      )}
     </article>
   );
 };
