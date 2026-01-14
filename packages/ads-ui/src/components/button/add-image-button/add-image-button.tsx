da86482ecfadfd0feaeb5b64b6b9efcd278d0986
eunkr82 <eun.kr82@gmail.com>
2026-01-15 00:59:55 +0900

    refactor: 리뷰 사항 반영

--- packages/ads-ui/src/components/button/add-image-button/add-image-button.tsx
+++ packages/ads-ui/src/components/button/add-image-button/add-image-button.tsx
@@ -9,20 +9,23 @@ interface AddImageButtonProps {
 
 const AddImageButton = ({ imageUrl, onClick }: AddImageButtonProps) => {
   const hasImage = Boolean(imageUrl);
+
+  const content = hasImage ? (
+    <img src={imageUrl} alt='첨부된 사진' className={styles.img} />
+  ) : (
+    <>
+      <CameraIcon className={styles.icon} />
+      <span>사진 첨부</span>
+    </>
+  );
   return (
     <button
       type='button'
+      aria-label={hasImage ? '첨부된 사진 변경' : '사진 첨부'}
       className={styles.addImageButton({ hasImage })}
       onClick={onClick}
     >
-      {hasImage ? (
-        <img src={imageUrl} alt='첨부된 사진' className={styles.img} />
-      ) : (
-        <>
-          <CameraIcon className={styles.icon} />
-          <span>사진 첨부</span>
-        </>
-      )}
+      {content}
     </button>
   );
 };
