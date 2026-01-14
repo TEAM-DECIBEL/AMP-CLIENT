aa7f8b96feb3bd9b49f0162541841de20fce331a
eunkr82 <eun.kr82@gmail.com>
2026-01-14 21:46:41 +0900

    chore: 테스트 파일 원상복구

--- apps/audience/src/pages/mypage/mypage.tsx
+++ apps/audience/src/pages/mypage/mypage.tsx
@@ -1,41 +1,5 @@
-import { useNavigate } from 'react-router';
-
-import { MyPageMenuContainer, MyPageMenuItem } from '@amp/ads-ui';
-
-import { ROUTE_PATH } from '@shared/constants/path';
-
 const MyPage = () => {
-  const navigate = useNavigate();
-
-  const MENU = [
-    {
-      id: 'my-events',
-      label: '내 관람 공연',
-      path: `/${ROUTE_PATH.MY_EVENTS}`,
-      disabled: false,
-    },
-    {
-      id: 'saved-notices',
-      label: '저장한 공지',
-      path: `/${ROUTE_PATH.SAVED_NOTICES}`,
-      disabled: true,
-    },
-  ];
-  return (
-    <div>
-      <MyPageMenuContainer>
-        {MENU.map((menu) => (
-          <MyPageMenuItem
-            key={menu.id}
-            disabled={menu.disabled}
-            onClick={() => navigate(menu.path)}
-          >
-            {menu.label}
-          </MyPageMenuItem>
-        ))}
-      </MyPageMenuContainer>
-    </div>
-  );
+  return <div>관객 마이페이지</div>;
 };
 
 export default MyPage;
