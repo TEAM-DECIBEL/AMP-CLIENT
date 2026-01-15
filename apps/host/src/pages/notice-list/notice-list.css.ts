9e316160b71a157278d93cc7c8f7c13e31e9b886
JS MacBook Pro <j_seokpark@naver.com>
2026-01-15 23:31:41 +0900

    feat: 주최 공지 뷰 레이아웃 스타일링

--- apps/host/src/pages/notice-list/notice-list.css.ts
+++ apps/host/src/pages/notice-list/notice-list.css.ts
@@ -16,4 +16,35 @@ export const tabBar = style({
   borderRadius: '16px 16px 0 0',
 });
 
-export const chipSection = style({});
+export const chipSection = style({
+  display: 'flex',
+  gap: '0.3rem',
+  padding: '1.6rem 2rem',
+  overflowX: 'auto',
+  whiteSpace: 'nowrap',
+  WebkitOverflowScrolling: 'touch',
+
+  selectors: {
+    '&::-webkit-scrollbar': {
+      display: 'none',
+    },
+  },
+  msOverflowStyle: 'none',
+  scrollbarWidth: 'none',
+});
+
+export const cardList = style({
+  padding: '0 2rem',
+});
+
+export const card = style({
+  marginBottom: '1.2rem',
+  paddingBottom: '1.2rem',
+  borderBottom: `1px solid ${ampThemeVars.color.gray_200}`,
+  selectors: {
+    '&:last-child': {
+      paddingBottom: '1.2rem',
+      borderBottom: 'none',
+    },
+  },
+});
--- apps/host/src/pages/notice-list/notice-list.tsx
+++ apps/host/src/pages/notice-list/notice-list.tsx
@@ -1,17 +1,55 @@
+import { useState } from 'react';
+
 import { NoticeBanner } from '@amp/ads-ui';
 import { Tabs } from '@amp/ads-ui';
+import { CategoryButton } from '@amp/ads-ui';
+import { CardNotice } from '@amp/ads-ui';
 
 import * as styles from './notice-list.css';
 
+const CATEGORIES = [
+  '전체',
+  '운영 시간',
+  '입장 안내',
+  'MD',
+  '이벤트',
+  '퇴근길',
+  '기타',
+] as const;
+
 const NoticeListPage = () => {
+  const [selectedCategory, setSelectedCategory] = useState<string>(
+    CATEGORIES[0],
+  );
+
+  const filteredNotices =
+    selectedCategory === '전체'
+      ? ANNOUNCEMENTS
+      : ANNOUNCEMENTS.filter((item) => item.categoryName === selectedCategory);
+
+  const sortedList = [...filteredNotices].sort((a, b) => {
+    if (a.isPinned && !b.isPinned) {
+      return -1;
+    }
+    if (!a.isPinned && b.isPinned) {
+      return 1;
+    }
+
+    return 0;
+  });
+
+  const handleChipClick = (category: string) => {
+    setSelectedCategory(category);
+  };
+
   return (
     <div>
       <div className={styles.bannerContainer}>
         <NoticeBanner
           dday={'D-5'}
           title={'Grand Mint Festival'}
-          location={'여의도공원'}
-          date={'2025-10-15'}
+          location={'여의도공원 일대'}
+          date={'2025.10.15 - 2025.10.18'}
         />
         <div className={styles.tabBar}>
           <Tabs defaultValue='notice' variant='notice'>
@@ -23,7 +61,36 @@ const NoticeListPage = () => {
         </div>
       </div>
       <main>
-        <div className={styles.chipSection}></div>
+        <div className={styles.chipSection}>
+          {CATEGORIES.map((category) => (
+            <CategoryButton
+              key={category}
+              variant='primary'
+              selected={selectedCategory === category}
+              onChange={() => handleChipClick(category)}
+            >
+              {category}
+            </CategoryButton>
+          ))}
+        </div>
+        <div className={styles.cardList}>
+          {sortedList.length === 0 ? (
+            <div>등록된 공지사항이 없습니다.</div>
+          ) : (
+            sortedList.map((notice) => (
+              <div key={notice.announcementId} className={styles.card}>
+                <CardNotice
+                  key={notice.announcementId}
+                  imageUrl={notice.imageUrl}
+                  title={notice.title}
+                  content={notice.content}
+                  isPinned={notice.isPinned}
+                  createdAt={notice.createdAt}
+                />
+              </div>
+            ))
+          )}
+        </div>
       </main>
     </div>
   );
