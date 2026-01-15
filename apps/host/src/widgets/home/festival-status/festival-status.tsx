d75fe0535f9b596675814c178b97e91820207f43
임지수 <dlawltn0708@naver.com>
2026-01-16 01:38:52 +0900

    feat: 삭제 모달 추가

--- apps/host/src/widgets/home/festival-status/festival-status.tsx
+++ apps/host/src/widgets/home/festival-status/festival-status.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import { OptionSheet } from '@amp/ads-ui';
+import { Modal, OptionSheet, RectButton } from '@amp/ads-ui';
 
 import type { Festival } from '@shared/types/home-response';
 
@@ -22,6 +22,7 @@ const FestivalStatus = ({
   upcomingFestivals,
 }: FestivalStatusProps) => {
   const [isOptionSheetOpen, setIsOptionSheetOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
 
   const handleOpenOptionSheet = () => {
     setIsOptionSheetOpen(true);
@@ -29,6 +30,19 @@ const FestivalStatus = ({
 
   const handleCloseOptionSheet = () => {
     setIsOptionSheetOpen(false);
+  };
+
+  const handleOpenDeleteModal = () => {
+    setIsOptionSheetOpen(false);
+    setIsDeleteModalOpen(true);
+  };
+
+  const handleCloseDeleteModal = () => {
+    setIsDeleteModalOpen(false);
+  };
+
+  const handleConfirmDelete = () => {
+    setIsDeleteModalOpen(false);
   };
 
   const sections = [
@@ -59,10 +73,25 @@ const FestivalStatus = ({
         <OptionSheet.Item onClick={handleCloseOptionSheet}>
           수정하기
         </OptionSheet.Item>
-        <OptionSheet.Item onClick={handleCloseOptionSheet}>
+        <OptionSheet.Item onClick={handleOpenDeleteModal}>
           삭제하기
         </OptionSheet.Item>
       </OptionSheet>
+      <Modal open={isDeleteModalOpen} onClose={handleCloseDeleteModal}>
+        <Modal.Panel role='alertdialog'>
+          <Modal.Content>
+            <Modal.Title>공연을 삭제하시겠어요?</Modal.Title>
+          </Modal.Content>
+          <Modal.Actions>
+            <RectButton variant='secondary' onClick={handleCloseDeleteModal}>
+              취소
+            </RectButton>
+            <RectButton variant='primary' onClick={handleConfirmDelete}>
+              삭제하기
+            </RectButton>
+          </Modal.Actions>
+        </Modal.Panel>
+      </Modal>
     </div>
   );
 };
