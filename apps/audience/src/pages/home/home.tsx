bb992feca12267230ea6a2e6644d00c2769d01c8
임지수 <dlawltn0708@naver.com>
2026-01-16 05:43:20 +0900

    feat: 홈 배너 추가

--- apps/audience/src/pages/home/home.tsx
+++ apps/audience/src/pages/home/home.tsx
@@ -1,5 +1,19 @@
+import HomeBanner from '@widgets/banner/home-banner/home-banner';
+
 const HomePage = () => {
-  return <div> 관객 홈페이지</div>;
+  return (
+    <div>
+      <HomeBanner
+        nickname='관객 이름'
+        status='card'
+        title='Grand Mint Festival'
+        location='올림픽공원 일대'
+        date='2025. 10. 18 - 2025. 10. 19'
+        dday={0}
+      />
+      관객 홈페이지
+    </div>
+  );
 };
 
 export default HomePage;
