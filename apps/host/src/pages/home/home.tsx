81e4da80bf4c53b6b9121e820f4e00974a7d384c
임지수 <dlawltn0708@naver.com>
2026-01-16 02:11:15 +0900

    feat: 네비게이션 연결

--- apps/host/src/pages/home/home.tsx
+++ apps/host/src/pages/home/home.tsx
@@ -1,7 +1,10 @@
+import { useNavigate } from 'react-router';
+
 import { CtaButton } from '@amp/ads-ui';
 
 import FestivalOverview from '@widgets/home/festival-overview/festival-overview';
 
+import { ROUTE_PATH } from '@shared/constants/path';
 import { homeData } from '@shared/mocks/home-data';
 import CardHomebannerOrg from '@shared/ui/card/card-homebanner-organizer/card-homebanner-org';
 import Tooltip from '@shared/ui/tooltip/tooltip';
@@ -13,8 +16,11 @@ const HomePage = () => {
 
   // 예시 닉네임
   const nickname = 'SOPT';
-  const showTooltip =
-    summary.ongoingCount === 0 && summary.upcomingCount === 0;
+  const showTooltip = summary.ongoingCount === 0 && summary.upcomingCount === 0;
+  const navigate = useNavigate();
+  const handleCreateClick = () => {
+    navigate(`/${ROUTE_PATH.EVENT_CREATE}`);
+  };
 
   return (
     <section className={styles.page}>
@@ -29,7 +35,7 @@ const HomePage = () => {
       </div>
       <div className={styles.ctaArea}>
         {showTooltip && <Tooltip />}
-        <CtaButton type='primary' onClick={() => {}}>
+        <CtaButton type='primary' onClick={handleCreateClick}>
           공연 등록하기
         </CtaButton>
       </div>
