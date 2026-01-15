1a9a8623f6853ff59f59fb101a0079e2421e38b1
임지수 <dlawltn0708@naver.com>
2026-01-16 06:12:38 +0900

    feat: 탭바 구현

--- apps/audience/src/pages/home/home.tsx
+++ apps/audience/src/pages/home/home.tsx
@@ -1,3 +1,5 @@
+import { Tabs } from '@amp/ads-ui';
+
 import HomeBanner from '@widgets/banner/home-banner/home-banner';
 
 import { homeData } from '@shared/mocks/home-data';
@@ -21,6 +23,12 @@ const HomePage = () => {
   return (
     <div>
       {banner}
+      <Tabs defaultValue='all' variant='viewer'>
+        <Tabs.List>
+          <Tabs.Trigger value='all'>전체 공연</Tabs.Trigger>
+          <Tabs.Trigger value='upcoming'>관람 예정 공연</Tabs.Trigger>
+        </Tabs.List>
+      </Tabs>
       관객 홈페이지
     </div>
   );
