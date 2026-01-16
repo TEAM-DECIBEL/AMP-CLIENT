fa6b27cce13039a062cde07d617617ffea04adae
임지수 <dlawltn0708@naver.com>
2026-01-16 21:56:18 +0900

    feat: ctaArea 그라데이션 추가

--- apps/host/src/pages/home/home.css.ts
+++ apps/host/src/pages/home/home.css.ts
@@ -28,5 +28,28 @@ export const ctaArea = style({
   flexDirection: 'column',
   alignItems: 'center',
   gap: '0.6rem',
-  background: `linear-gradient(180deg, rgba(255, 255, 255, 0.15) 0%, ${ampThemeVars.color.gray_000} 30%, ${ampThemeVars.color.gray_000} 100%)`,
+  backgroundColor: ampThemeVars.color.gray_000,
+  selectors: {
+    '&::before': {
+      content: '""',
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      top: 'calc(-10rem - 1px)',
+      height: 'calc(10rem + 2px)',
+      background: `linear-gradient(
+    180deg,
+    rgba(255, 255, 255, 0) 0%,
+    rgba(255, 255, 255, 0) 15%,
+    rgba(255, 255, 255, 0.08) 30%,
+    rgba(255, 255, 255, 0.22) 50%,
+    rgba(255, 255, 255, 0.55) 70%,
+    rgba(255, 255, 255, 0.9) 85%,
+    rgba(255, 255, 255, 1) 95%,
+    ${ampThemeVars.color.gray_000} 100%
+  )`,
+
+      pointerEvents: 'none',
+    },
+  },
 });
