e133444e13066ee16828581b9f977eaec1022277
임지수 <dlawltn0708@naver.com>
2026-01-18 17:56:34 +0900

    fix: myHistoryFestivals에 타입 명시

--- apps/host/src/shared/mocks/my-history-data.ts
+++ apps/host/src/shared/mocks/my-history-data.ts
@@ -1,4 +1,6 @@
-export const myHistoryFestivals = [
+import type { FestivalBase } from '@shared/types/festival';
+
+export const myHistoryFestivals: FestivalBase[] = [
   {
     festivalId: 22,
     mainImageUrl:
