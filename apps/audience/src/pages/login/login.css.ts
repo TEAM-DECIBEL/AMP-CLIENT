e9e2312f8467f2359e1d46430b87091e9521ee71
Sohyunnnn <thgus05061@naver.com>
2026-01-19 03:09:43 +0900

    feat: audience 로그인 페이지 구현

---
+++ apps/audience/src/pages/login/login.css.ts
@@ -0,0 +1,24 @@
+import { style } from '@vanilla-extract/css';
+
+import { ampThemeVars } from '@amp/ads-ui/styles';
+
+export const container = style({
+  height: '100dvh',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
+
+export const ctaButtonContainer = style({
+  padding: '2rem',
+});
+
+export const skipButton = style({
+  ...ampThemeVars.font.body_r_13,
+  color: ampThemeVars.color.gray_700,
+  textAlign: 'center',
+  marginTop: '2rem',
+  textDecoration: 'underline',
+  display: 'inline-flex',
+});
