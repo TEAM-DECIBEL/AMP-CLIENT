1f18b367eb44c2791fcb30f23f85a072dbf83c77
임지수 <dlawltn0708@naver.com>
2026-01-18 18:14:26 +0900

    feat: 축제 리스트 컴포넌트 분리

--- apps/audience/src/pages/my-events/my-events.tsx
+++ apps/audience/src/pages/my-events/my-events.tsx
@@ -1,42 +1,14 @@
-import { CardFestival, Chip } from '@amp/ads-ui';
+import FestivalList from '@widgets/my-events/festival-list';
 
 import { myEventsData } from '@shared/mocks/my-events-data';
 
 import * as styles from './my-events.css';
 
-const getStatusChip = (status: string) => {
-  if (status === '관람 중') {
-    return (
-      <Chip variant='status' status='current'>
-        관람 중
-      </Chip>
-    );
-  }
-
-  return (
-    <Chip variant='status' status='upcoming'>
-      관람 예정
-    </Chip>
-  );
-};
-
 const MyEventsPage = () => {
   return (
     <section className={styles.page}>
       <div className={styles.list}>
-        {myEventsData.festivals.map((festival) => (
-          <CardFestival key={festival.festivalId}>
-            <CardFestival.Image
-              src={festival.mainImageUrl}
-              alt={festival.title}
-            />
-            <CardFestival.Body title={festival.title} date={festival.period}>
-              <CardFestival.Chip>
-                {getStatusChip(festival.status)}
-              </CardFestival.Chip>
-            </CardFestival.Body>
-          </CardFestival>
-        ))}
+        <FestivalList festivals={myEventsData.festivals} />
       </div>
     </section>
   );
