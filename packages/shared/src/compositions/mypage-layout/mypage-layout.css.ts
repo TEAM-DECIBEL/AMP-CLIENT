19aebca8e004bd51314cc8b967b954708cd1f762
임지수 <dlawltn0708@naver.com>
2026-01-18 19:56:23 +0900

    fix: 이미지 크기 수정

--- packages/shared/src/compositions/mypage-layout/mypage-layout.css.ts
+++ packages/shared/src/compositions/mypage-layout/mypage-layout.css.ts
@@ -25,8 +25,8 @@ export const profileImage = style({
 });
 
 export const profilePlaceholder = style({
-  width: '10.4rem',
-  height: '10.4rem',
+  width: '9.2rem',
+  height: '9.2rem',
   borderRadius: '50%',
   backgroundColor: ampThemeVars.color.gray_100,
   border: `1px solid ${ampThemeVars.color.gray_200}`,
