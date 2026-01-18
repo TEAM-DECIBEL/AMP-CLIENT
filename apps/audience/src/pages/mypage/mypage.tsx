40725d663b7e4c691e116a1c9527de7f2f7554ef
임지수 <dlawltn0708@naver.com>
2026-01-18 16:41:42 +0900

    refactor: 메뉴 상수 분리

--- apps/audience/src/pages/mypage/mypage.tsx
+++ apps/audience/src/pages/mypage/mypage.tsx
@@ -6,23 +6,28 @@ import { ROUTE_PATH } from '@shared/constants/path';
 
 const MyPage = () => {
   const navigate = useNavigate();
+  const menuItems = [
+    {
+      id: 'my-events',
+      label: '내 관람 공연',
+      path: `/${ROUTE_PATH.MY_EVENTS}`,
+    },
+    {
+      id: 'saved-notices',
+      label: '저장한 공지',
+      path: `/${ROUTE_PATH.SAVED_NOTICES}`,
+    },
+  ] as const;
 
   return (
     <MyPageLayout
       name='관객 이름'
       roleLabel='관객'
-      menuItems={[
-        {
-          id: 'my-events',
-          label: '내 관람 공연',
-          onClick: () => navigate(`/${ROUTE_PATH.MY_EVENTS}`),
-        },
-        {
-          id: 'saved-notices',
-          label: '저장한 공지',
-          onClick: () => navigate(`/${ROUTE_PATH.SAVED_NOTICES}`),
-        },
-      ]}
+      menuItems={menuItems.map((menu) => ({
+        id: menu.id,
+        label: menu.label,
+        onClick: () => navigate(menu.path),
+      }))}
     />
   );
 };
