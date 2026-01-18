b4fa5aa318e3b61d295c87b192d4ece97670efc0
임지수 <dlawltn0708@naver.com>
2026-01-18 16:35:14 +0900

    feat: 관객 마이페이지 구현

--- apps/audience/src/pages/mypage/mypage.tsx
+++ apps/audience/src/pages/mypage/mypage.tsx
@@ -1,5 +1,30 @@
+import { useNavigate } from 'react-router';
+
+import { MyPageLayout } from '@amp/shared';
+
+import { ROUTE_PATH } from '@shared/constants/path';
+
 const MyPage = () => {
-  return <div>관객 마이페이지</div>;
+  const navigate = useNavigate();
+
+  return (
+    <MyPageLayout
+      name='관객 이름'
+      roleLabel='관객'
+      menuItems={[
+        {
+          id: 'my-events',
+          label: '내 관람 공연',
+          onClick: () => navigate(`/${ROUTE_PATH.MY_EVENTS}`),
+        },
+        {
+          id: 'saved-notices',
+          label: '저장한 공지',
+          onClick: () => navigate(`/${ROUTE_PATH.SAVED_NOTICES}`),
+        },
+      ]}
+    />
+  );
 };
 
 export default MyPage;
