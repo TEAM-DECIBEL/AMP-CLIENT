af667e5b81fb4d9adb9165e065198b028bbef38d
임지수 <dlawltn0708@naver.com>
2026-01-18 18:16:19 +0900

    feat: 엠티뷰 추가

--- apps/audience/src/pages/my-events/my-events.tsx
+++ apps/audience/src/pages/my-events/my-events.tsx
@@ -1,3 +1,5 @@
+import { EmptyView } from '@amp/ads-ui';
+
 import FestivalList from '@widgets/my-events/festival-list';
 
 import { myEventsData } from '@shared/mocks/my-events-data';
@@ -5,6 +7,16 @@ import { myEventsData } from '@shared/mocks/my-events-data';
 import * as styles from './my-events.css';
 
 const MyEventsPage = () => {
+  if (myEventsData.festivals.length === 0) {
+    return (
+      <section className={styles.page}>
+        <div className={styles.empty}>
+          <EmptyView title='관람 공연이 없어요!' />
+        </div>
+      </section>
+    );
+  }
+
   return (
     <section className={styles.page}>
       <div className={styles.list}>
