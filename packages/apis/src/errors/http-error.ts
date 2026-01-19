0ee0fb5654ea33d587f1cf1168be643796c1b7f8
임지수 <dlawltn0708@naver.com>
2026-01-19 22:45:58 +0900

    refactor: 매핑 테이블로 변경

--- packages/apis/src/errors/http-error.ts
+++ packages/apis/src/errors/http-error.ts
@@ -1,36 +1,20 @@
 import { HTTP_STATUS_CODE } from '../constants/http-status';
 
+const STATUS_NAME_MAP: Record<number, string> = {
+  [HTTP_STATUS_CODE.BAD_REQUEST]: 'BAD_REQUEST',
+  [HTTP_STATUS_CODE.UNAUTHORIZED]: 'UNAUTHORIZED',
+  [HTTP_STATUS_CODE.NOT_FOUND]: 'NOT_FOUND',
+  [HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR]: 'INTERNAL_SERVER_ERROR',
+};
+
 export class HTTPError extends Error {
   statusCode: number;
   code?: number;
 
   constructor(statusCode: number, message?: string, code?: number) {
     super(message);
-    let name = 'HTTPError';
-
-    switch (statusCode) {
-      case HTTP_STATUS_CODE.BAD_REQUEST: {
-        name += ': BAD_REQUEST';
-        break;
-      }
-      case HTTP_STATUS_CODE.UNAUTHORIZED: {
-        name += ': UNAUTHORIZED';
-        break;
-      }
-      case HTTP_STATUS_CODE.NOT_FOUND: {
-        name += ': NOT_FOUND';
-        break;
-      }
-      case HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR: {
-        name += ': INTERNAL_SERVER_ERROR';
-        break;
-      }
-      default: {
-        break;
-      }
-    }
-
-    this.name = name;
+    const statusName = STATUS_NAME_MAP[statusCode];
+    this.name = statusName ? `HTTPError: ${statusName}` : 'HTTPError';
     this.statusCode = statusCode;
     this.code = code;
   }
