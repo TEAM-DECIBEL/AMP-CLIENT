11d53ff65be1fa544a31e38ef1260d55e5212b3a
JS MacBook Pro <j_seokpark@naver.com>
2026-01-19 15:41:41 +0900

    feat: host pwa 적용

--- apps/host/vite.config.ts
+++ apps/host/vite.config.ts
@@ -2,6 +2,7 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 import { baseViteConfig } from '@amp/vite-config';
 
@@ -14,5 +15,44 @@ export default defineConfig(
     server: {
       port: 5174,
     },
+
+    plugins: [
+      VitePWA({
+        registerType: 'autoUpdate',
+        devOptions: {
+          enabled: true,
+        },
+
+        includeAssets: [
+          'favicon.svg',
+          'amp-pwa-logo-192.png',
+          'amp-pwa-logo-512.png',
+        ],
+        manifest: {
+          name: 'AMP-HOST',
+          short_name: 'AMP',
+          description: '공연 공지를 가장 가까이에서',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'amp-pwa-logo-192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'amp-pwa-logo-512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: 'amp-pwa-logo-512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
+          ],
+        },
+      }),
+    ],
   }),
 );
