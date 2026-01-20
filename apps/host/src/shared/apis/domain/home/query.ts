3242dcb65f262841f4120ecae995d3ac1d7d59af
임지수 <dlawltn0708@naver.com>
2026-01-21 04:49:58 +0900

    fix: 코드리뷰 반영

--- apps/host/src/shared/apis/domain/home/query.ts
+++ apps/host/src/shared/apis/domain/home/query.ts
@@ -1,10 +1,10 @@
 import { queryOptions } from '@tanstack/react-query';
 
+import type { SuccessResponse } from '@amp/apis';
 import { instance } from '@amp/apis';
 
 import { END_POINT } from '@shared/constants/end-point';
 import { ORGANIZERS_QUERY_KEY } from '@shared/constants/query-key';
-import type { ApiResponse } from '@shared/types/api-response';
 import type { HomeResponseData } from '@shared/types/home-response';
 
 export const getHomeFestivals = async (
@@ -12,8 +12,8 @@ export const getHomeFestivals = async (
     page?: number;
     size?: number;
   } = {},
-) => {
-  const { data } = await instance.get<ApiResponse<HomeResponseData>>(
+): Promise<HomeResponseData | null> => {
+  const { data } = await instance.get<SuccessResponse<HomeResponseData>>(
     END_POINT.GET_HOME_FESTIVALS,
     { params },
   );
