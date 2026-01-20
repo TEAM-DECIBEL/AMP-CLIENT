bdfbd0e6edb7d40ba7561ae1f42feed6feb2af7e
임지수 <dlawltn0708@naver.com>
2026-01-20 10:34:06 +0900

    fix: 쿼리 함수 수정

--- apps/host/src/shared/apis/domain/home/query.ts
+++ apps/host/src/shared/apis/domain/home/query.ts
@@ -4,6 +4,7 @@ import { instance } from '@amp/apis';
 
 import { END_POINT } from '@shared/constants/end-point';
 import { ORGANIZERS_QUERY_KEY } from '@shared/constants/query-key';
+import type { ApiResponse } from '@shared/types/api-response';
 import type { HomeResponseData } from '@shared/types/home-response';
 
 export const getHomeFestivals = async (
@@ -12,11 +13,11 @@ export const getHomeFestivals = async (
     size?: number;
   } = {},
 ) => {
-  const { data } = await instance.get<HomeResponseData>(
+  const { data } = await instance.get<ApiResponse<HomeResponseData>>(
     END_POINT.GET_HOME_FESTIVALS,
     { params },
   );
-  return data;
+  return data?.data ?? null;
 };
 
 export const HOME_QUERY_OPTIONS = {
