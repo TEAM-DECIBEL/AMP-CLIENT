6ff18bdd9213ac6c0d7fdf9066bf0ba26546b8b3
Sohyunnnn <thgus05061@naver.com>
2026-01-21 00:54:39 +0900

    rename: 네이밍 수정

--- apps/audience/src/shared/assets/index.ts
+++ apps/audience/src/shared/assets/index.ts
@@ -1,9 +1,9 @@
 import notFoundImage from './404.webp';
 import loginImage from './login.webp';
-import OnboardingImage from './onboarding.webp';
+import onboardingImage from './onboarding.webp';
 
 export const IMAGES = {
   LOGIN: loginImage,
   NOT_FOUND: notFoundImage,
-  ONBOARDING: OnboardingImage,
+  ONBOARDING: onboardingImage,
 } as const;
