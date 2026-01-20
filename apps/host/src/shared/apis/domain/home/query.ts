e704519b452a6c80bb6226edef12b5d12bb95937
임지수 <dlawltn0708@naver.com>
2026-01-20 10:11:29 +0900

    feat: 쿼리 함수 추가

--- apps/host/src/shared/apis/domain/home/query.ts
+++ apps/host/src/shared/apis/domain/home/query.ts
@@ -6,10 +6,12 @@ import { END_POINT } from '@shared/constants/end-point';
 import { ORGANIZERS_QUERY_KEY } from '@shared/constants/query-key';
 import type { HomeResponseData } from '@shared/types/home-response';
 
-export const getHomeFestivals = async (params: {
-  page?: number;
-  size?: number;
-}) => {
+export const getHomeFestivals = async (
+  params: {
+    page?: number;
+    size?: number;
+  } = {},
+) => {
   const { data } = await instance.get<HomeResponseData>(
     END_POINT.GET_HOME_FESTIVALS,
     { params },
@@ -18,7 +20,7 @@ export const getHomeFestivals = async (params: {
 };
 
 export const HOME_QUERY_OPTIONS = {
-  FESTIVALS: (params: { page?: number; size?: number }) =>
+  FESTIVALS: (params: { page?: number; size?: number } = {}) =>
     queryOptions({
       queryKey: [...ORGANIZERS_QUERY_KEY.HOME_FESTIVALS(), params],
       queryFn: () => getHomeFestivals(params),
