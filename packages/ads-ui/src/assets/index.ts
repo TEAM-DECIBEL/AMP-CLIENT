682e24b3fd4e9102cac10019b8d7f711168d66c1
Sohyunnnn <thgus05061@naver.com>
2026-01-21 00:29:29 +0900

    rename: 파일 이동

--- packages/ads-ui/src/assets/index.ts
+++ packages/ads-ui/src/assets/index.ts
@@ -1,7 +1,5 @@
-import cardHome from './card-home.webp';
 import logo from './logo.webp';
 
 export const IMAGES = {
-  CARD_HOME: cardHome,
   LOGO: logo,
 } as const;
--- packages/compositions/src/assets/index.ts
+++ packages/compositions/src/assets/index.ts
@@ -1,5 +1,7 @@
+import cardHome from './card-home.webp';
 import noticeBanner from './notice-banner.webp';
 
 export const IMAGES = {
   NOTICE_BANNER: noticeBanner,
+  CARD_HOME: cardHome,
 } as const;
