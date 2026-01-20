0d58d52ae6ad1bdc1c836e8d0ba8cf65c877f552
임지수 <dlawltn0708@naver.com>
2026-01-21 01:53:24 +0900

    feat: 엔드포인트 추가

---
+++ apps/audience/src/shared/constants/end-point.ts
@@ -0,0 +1,6 @@
+export const END_POINT = {
+  // 홈 화면
+  GET_ALL_FESTIVALS: '/users/festivals',
+  GET_PLANNED_FESTIVALS: '/users/me/festivals',
+  GET_UPCOMING_FESTIVAL: '/users/me/festivals/recent',
+};
