2df6273a5f786c60feae68c174fccae0cd7f3917
임지수 <dlawltn0708@naver.com>
2026-01-21 12:13:53 +0900

    chore: 폴더구조 변경

--- apps/host/src/features/home/apis/query.ts
+++ apps/host/src/features/home/apis/query.ts
@@ -1,11 +1,11 @@
 import { queryOptions } from '@tanstack/react-query';
 
 import { get } from '@amp/apis';
+import { PageSizeParams } from '@amp/shared/types';
 
 import { END_POINT } from '@shared/constants/end-point';
 import { ORGANIZERS_QUERY_KEY } from '@shared/constants/query-key';
 import type { HomeResponseData } from '@shared/types/home-response';
-import type { PageSizeParams } from '@shared/types/page-size-params';
 
 export const getHomeFestivals = (params: PageSizeParams = {}) =>
   get<HomeResponseData, PageSizeParams>(END_POINT.GET_HOME_FESTIVALS, params);
