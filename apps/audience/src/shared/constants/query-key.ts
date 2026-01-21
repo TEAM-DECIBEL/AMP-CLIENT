b5eb5fc57a60351b25bf77c111f459ff2acec5c7
임지수 <dlawltn0708@naver.com>
2026-01-22 03:20:52 +0900

    feat: 쿼리키 추가

---
+++ apps/audience/src/shared/constants/query-key.ts
@@ -0,0 +1,13 @@
+export const USERS_QUERY_KEY = {
+  ALL: ['users'],
+  HOME_FESTIVALS_ALL: () => [...USERS_QUERY_KEY.ALL, 'home-festivals-all'],
+  HOME_FESTIVALS_PLANNED: () => [
+    ...USERS_QUERY_KEY.ALL,
+    'home-festivals-planned',
+  ],
+  HOME_FESTIVAL_UPCOMING: () => [
+    ...USERS_QUERY_KEY.ALL,
+    'home-festivals-upcoming',
+  ],
+  MY_PAGE: () => [...USERS_QUERY_KEY.ALL, 'my-page'],
+} as const;
