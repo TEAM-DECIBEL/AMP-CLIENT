1df9795cea304a654c0ac9b05d3764a233a10729
임지수 <dlawltn0708@naver.com>
2026-01-21 12:06:29 +0900

    chore: 임시 토큰 로직 제거

--- packages/apis/src/http/instance.ts
+++ packages/apis/src/http/instance.ts
@@ -8,9 +8,6 @@ const baseURL = ENV.API_BASE_URL;
 export const instance = axios.create({
   baseURL,
   withCredentials: true,
-  headers: ENV.AUTH_TOKEN
-    ? { Authorization: `Bearer ${ENV.AUTH_TOKEN}` }
-    : undefined,
 });
 
 instance.interceptors.response.use(
