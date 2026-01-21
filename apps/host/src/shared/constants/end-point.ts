769c78ba9864428ec49b04d761de18e3f0c86e78
JS MacBook Pro <j_seokpark@naver.com>
2026-01-21 19:58:11 +0900

    feat: 엔드포인트, 쿼리키, 응답 타입 설정

--- apps/host/src/shared/constants/end-point.ts
+++ apps/host/src/shared/constants/end-point.ts
@@ -1,4 +1,8 @@
 export const END_POINT = {
   // 홈 화면
   GET_HOME_FESTIVALS: '/organizer/me/festivals/active',
+
+  // 공지 상세
+  GET_FESTIVAL_NOTICE_DETAIL: (noticeId: number | string) =>
+    `/common/notices/${noticeId}`,
 } as const;
--- apps/host/src/shared/constants/query-key.ts
+++ apps/host/src/shared/constants/query-key.ts
@@ -1,4 +1,9 @@
 export const ORGANIZERS_QUERY_KEY = {
-  ALL: ["organizers"],
-  HOME_FESTIVALS: () => [...ORGANIZERS_QUERY_KEY.ALL, "home-festivals"],
+  ALL: ['organizers'],
+  HOME_FESTIVALS: () => [...ORGANIZERS_QUERY_KEY.ALL, 'home-festivals'],
+  FESTIVAL_NOTICE_DETAIL: (noticeId: number | string) => [
+    ...ORGANIZERS_QUERY_KEY.ALL,
+    'festival-notice-detail',
+    noticeId,
+  ],
 } as const;
