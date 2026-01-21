473636388633e4eb8b4bf7728cfe324846587d62
JS MacBook Pro <j_seokpark@naver.com>
2026-01-21 20:05:47 +0900

    feat: 주최 공지 세부 API 연동

--- apps/host/src/shared/constants/path.ts
+++ apps/host/src/shared/constants/path.ts
@@ -2,7 +2,7 @@ export const ROUTE_PATH = {
   EVENT_CREATE: '/events/new',
   NOTICE_LIST: '/events/:eventId/notices',
   NOTICE_CREATE: '/events/:eventId/notices/new',
-  NOTICE_DETAILS: '/events/:eventId/notices/:noticeId',
+  NOTICE_DETAILS: '/common/notices/:noticeId',
   MYPAGE: '/mypage',
   ONBOARDING: '/onboarding',
   MY_HISTORY: '/mypage/history',
