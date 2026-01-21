b41a4ddd59825d57dd4fc0cf2d58d0f04ae56b16
임지수 <dlawltn0708@naver.com>
2026-01-21 18:39:28 +0900

    feat: 쿼리키 및 응답 타입 추가

--- apps/host/src/shared/constants/query-key.ts
+++ apps/host/src/shared/constants/query-key.ts
@@ -1,4 +1,5 @@
 export const ORGANIZERS_QUERY_KEY = {
   ALL: ["organizers"],
   HOME_FESTIVALS: () => [...ORGANIZERS_QUERY_KEY.ALL, "home-festivals"],
+  MY_PAGE: () => [...ORGANIZERS_QUERY_KEY.ALL, "my-page"],
 } as const;
