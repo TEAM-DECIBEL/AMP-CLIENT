984367cbc22b58954069e99dfd8d85ffb5e72209
Sohyunnnn <thgus05061@naver.com>
2026-01-22 02:09:19 +0900

    refactor: 타입 명시

--- apps/host/src/features/onboarding/query.ts
+++ apps/host/src/features/onboarding/query.ts
@@ -4,4 +4,4 @@ import { END_POINT } from '@shared/constants/end-point';
 import { OnboardingCompleteBody } from '@shared/types/onboarding';
 
 export const postOnboardingComplete = (body: OnboardingCompleteBody) =>
-  post<OnboardingCompleteBody>(END_POINT.POST_ONBOARDING_COMPLETE, body);
+  post<void, OnboardingCompleteBody>(END_POINT.POST_ONBOARDING_COMPLETE, body);
