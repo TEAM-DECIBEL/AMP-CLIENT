b7864714fcd5c49eb7204d0a2a92641914024b41
임지수 <dlawltn0708@naver.com>
2026-01-22 16:58:07 +0900

    fix: 병합 충돌 해결

--- apps/host/src/shared/constants/end-point.ts
+++ apps/host/src/shared/constants/end-point.ts
@@ -12,16 +12,6 @@ export const END_POINT = {
   // 공지 수정
   PUT_NOTICE: (noticeId: number) => `/organizer/notices/${noticeId}`,
 
-  // 공지 업로드
-  POST_FESTIVAL_NOTICE: (festivalId: number) =>
-    `/organizer/festivals/${festivalId}/notices`,
-
-  // 공지 상세
-  GET_NOTICE_DETAIL: (noticeId: number) => `/common/notices/${noticeId}`,
-
-  // 공지 수정
-  PUT_NOTICE: (noticeId: number) => `/organizer/notices/${noticeId}`,
-
   // 주최 공지
   GET_FESTIVAL_NOTICES: (eventId: number) =>
     `/common/festivals/${eventId}/notices`,
