aa2a8238d75cd8b2ed6c5c18a9bba5c9c357d85a
임지수 <dlawltn0708@naver.com>
2026-01-22 13:44:40 +0900

    feat: 쿼리키 추가

--- apps/host/src/shared/constants/query-key.ts
+++ apps/host/src/shared/constants/query-key.ts
@@ -1,4 +1,10 @@
 export const ORGANIZERS_QUERY_KEY = {
   ALL: ['organizers'],
   HOME_FESTIVALS: () => [...ORGANIZERS_QUERY_KEY.ALL, 'home-festivals'],
+  NOTICE_CREATE: (festivalId: number) => [
+    ...ORGANIZERS_QUERY_KEY.ALL,
+    'festival',
+    festivalId,
+    'notice-create',
+  ],
 } as const;
