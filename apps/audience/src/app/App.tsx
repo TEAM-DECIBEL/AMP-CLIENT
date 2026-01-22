97c6f3f8cd3989515eb4272adbfac05c04cec90c
Sohyunnnn <thgus05061@naver.com>
2026-01-22 17:35:19 +0900

    feat: 리스너 추가

--- apps/audience/src/app/App.tsx
+++ apps/audience/src/app/App.tsx
@@ -1,12 +1,23 @@
+import { useEffect } from 'react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router';
+import { listenForegroundMessages } from 'src/push-foreground';
 
 import { queryClient } from '@shared/apis/query-client';
 
 import { router } from './router/router';
 
 function App() {
+  useEffect(() => {
+    listenForegroundMessages((payload) => {
+      console.log('[FCM][foreground]', payload);
+
+      //TODO 
+      // queryClient.invalidateQueries({ queryKey: ['notifications'] });
+    });
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
