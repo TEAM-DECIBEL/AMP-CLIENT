88dab8903d9ab5b262f7f8619c7050f59796d432
eunkr82 <eun.kr82@gmail.com>
2026-01-23 16:39:52 +0900

    fix: 주최사 마이페이지 추가

--- packages/ads-ui/src/assets/index.ts
+++ packages/ads-ui/src/assets/index.ts
@@ -1,11 +1,13 @@
+// import emptyAlert from './empty-alert.webp';
+import avatar from './avatar.webp';
 import cardHome from './card-home.webp';
 import crowdedBtn from './congestion-crowded-btn.webp';
 import normalBtn from './congestion-normal-btn.webp';
 import smoothBtn from './congestion-smooth-btn.webp';
-import emptyAlert from './empty-alert.webp';
 import logo from './logo.webp';
 
 export const IMAGES = {
+  AVATAR: avatar,
   CARD_HOME: cardHome,
   LOGO: logo,
   CROWDED_BUTTON: crowdedBtn,
