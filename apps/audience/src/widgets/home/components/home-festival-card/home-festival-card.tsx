1a903900765dc1af877ce71028021b19ebe3cd07
JS MacBook Pro <j_seokpark@naver.com>
2026-02-25 20:30:00 +0900

    refactor: 타입 import로 변경 및 빈 문자열 폴백 제거

--- apps/audience/src/widgets/home/components/home-festival-card/home-festival-card.tsx
+++ apps/audience/src/widgets/home/components/home-festival-card/home-festival-card.tsx
@@ -4,7 +4,7 @@ import { FestivalStatusGroup } from '@amp/compositions';
 import { useToggleWishListMutation } from '@features/notice-list/hooks/use-toggle-wishlist-mutation';
 
 import formatDday from '@shared/libs/format-dday';
-import {
+import type {
   AllFestivalItem,
   UpcomingFestivalItem,
 } from '@shared/types/home-response';
@@ -23,7 +23,7 @@ const HomeFestivalCard = ({ festival, onClick }: HomeFestivalCardProps) => {
 
   return (
     <CardFestival onClick={onClick}>
-      <CardFestival.Image src={mainImageUrl ?? ''} alt={title} />
+      <CardFestival.Image src={mainImageUrl} alt={title} />
       <CardFestival.Body title={title} date={period}>
         <CardFestival.Chip>
           <FestivalStatusGroup dDay={formatDday(dDay)} isWishlist={wishList} />
