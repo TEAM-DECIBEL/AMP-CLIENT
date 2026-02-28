7c14aecc73b591867908936312ec27a7bc57f7cb
Sohyunnnn <thgus05061@naver.com>
2026-02-28 14:46:21 +0900

    remove: 불필요한 env 제거

--- packages/apis/src/constants/env.ts
+++ packages/apis/src/constants/env.ts
@@ -1,5 +1,3 @@
 export const ENV = {
   API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
-  HOST_BASE_URL: import.meta.env.VITE_HOST_BASE_URL,
-  AUDIENCE_BASE_URL: import.meta.env.VITE_AUDIENCE_BASE_URL,
 } as const;
