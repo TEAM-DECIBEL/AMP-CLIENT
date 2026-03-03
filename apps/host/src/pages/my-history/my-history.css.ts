65fed915f995e96509f5ddb8a528d448d22da57b
JS MacBook Pro <j_seokpark@naver.com>
2026-03-04 01:31:34 +0900

    style: 스타일 속성 순서 변경

--- apps/host/src/pages/my-history/my-history.css.ts
+++ apps/host/src/pages/my-history/my-history.css.ts
@@ -3,9 +3,9 @@ import { style } from '@vanilla-extract/css';
 export const page = style({
   display: 'flex',
   flexDirection: 'column',
+  gap: '1.2rem',
   padding: '2rem',
   minHeight: '100dvh',
-  gap: '1.2rem',
 });
 
 export const empty = style({
