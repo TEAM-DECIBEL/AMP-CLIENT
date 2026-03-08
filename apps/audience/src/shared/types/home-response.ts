26c4e204f77b6b57570947129d590ce194d77ede
JS MacBook Pro <j_seokpark@naver.com>
2026-03-09 00:37:51 +0900

    fix: dDay로 필드명 통일

--- apps/audience/src/shared/types/home-response.ts
+++ apps/audience/src/shared/types/home-response.ts
@@ -12,7 +12,7 @@ export interface UpcomingFestivalResponse {
   location: string;
   startDate: string;
   endDate: string;
-  dday: number;
+  dDay: number;
 }
 
 export interface NicknameResponse {
