66ce633c5ed9ecc97d9322a841f4a4b40e93a4ce
eunkr82 <eun.kr82@gmail.com>
2026-03-11 12:58:03 +0900

    feat: 엔드포인트 추가

--- apps/host/src/shared/constants/end-point.ts
+++ apps/host/src/shared/constants/end-point.ts
@@ -1,6 +1,7 @@
 export const END_POINT = {
   // Festival
   POST_FESTIVAL_CREATE: '/festivals', // 공연 등록 화면
+  PATCH_FESTIVAL: (festivalId: number) => `/festivals/${festivalId}`, // 공연 수정 화면
   DELETE_FESTIVAL: (festivalId: number) => `/festivals/${festivalId}`, // 공연 삭제
   GET_FESTIVAL_BANNER: (festivalId: number) => `/festivals/${festivalId}`, // 공연 정보 조회
   GET_FESTIVAL_CONGESTION: (eventId: number) =>
