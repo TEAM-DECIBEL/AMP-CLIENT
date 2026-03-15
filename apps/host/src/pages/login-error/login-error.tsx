e56ee41d7cddf46223fbb3a78e7ef6481079cf0e
Sohyunnnn <thgus05061@naver.com>
2026-03-15 21:23:22 +0900

    rename: 네이밍 수정

--- apps/host/src/pages/login-error/login-error.tsx
+++ apps/host/src/pages/login-error/login-error.tsx
@@ -10,7 +10,7 @@ import * as styles from './login-error.css';
 const LoginError = () => {
   const navigate = useNavigate();
 
-  const handleButtonClick = () => {
+  const handleBackClick = () => {
     navigate(-1);
   };
   return (
@@ -21,7 +21,7 @@ const LoginError = () => {
         image={<img src={IMAGES.LOGIN_ERROR} alt='' />}
       />
       <div className={styles.ctaButtonContainer}>
-        <CtaButton type='common' color='gray' onClick={handleButtonClick}>
+        <CtaButton type='common' color='gray' onClick={handleBackClick}>
           돌아가기
         </CtaButton>
       </div>
