01b48bde0fd061a3121de890d440e9e37cc96060
Sohyunnnn <thgus05061@naver.com>
2026-03-16 23:06:00 +0900

    asset: add assets

--- packages/compositions/src/assets/index.ts
+++ packages/compositions/src/assets/index.ts
@@ -6,6 +6,10 @@ import logo from './logo.webp';
 import none from './none.webp';
 import normal from './normal.webp';
 import noticeBanner from './notice-banner.webp';
+import pwaStep1 from './pwa-step1.webp';
+import pwaStep2 from './pwa-step2.webp';
+import pwaStep3 from './pwa-step3.webp';
+import pwaStep4 from './pwa-step4.webp';
 import smooth from './smooth.webp';
 
 export const IMAGES = {
@@ -21,4 +25,9 @@ export const IMAGES = {
   EMPTY_NOTICE: emptyNotice,
 
   LOGO: logo,
+
+  PWA_STEP1: pwaStep1,
+  PWA_STEP2: pwaStep2,
+  PWA_STEP3: pwaStep3,
+  PWA_STEP4: pwaStep4,
 } as const;
