aad73beceec044737bdda7ccbc4c8c71c9ca23ac
Sohyunnnn <thgus05061@naver.com>
2026-03-16 22:08:32 +0900

    chore: export 추가

--- packages/shared/src/hooks/index.ts
+++ packages/shared/src/hooks/index.ts
@@ -1,2 +1,4 @@
+export { dismissInstallGuideForToday } from './install-guide-storage';
+export { shouldShowInstallGuide } from './install-guide-storage';
 export { useDraggableCarousel } from './use-draggable-carousel';
 export { useNoticeList } from './use-notice-list';
