faeda230c10270a9ee01eb7c4e710baf9c6bf726
eunkr82 <eun.kr82@gmail.com>
2026-03-19 06:58:58 +0900

    Revert "feat: clear 함수 추가"
    This reverts commit a8142b488dd47467ca9f60562ba3a4a5dd4602b2.

--- apps/host/src/shared/hooks/use-object-url/use-object-url.ts
+++ apps/host/src/shared/hooks/use-object-url/use-object-url.ts
@@ -3,30 +3,13 @@ import { useEffect, useState } from 'react';
 interface UseObjectUrlReturn {
   url: string;
   setFile: (file: File) => void;
-  clear: () => void;
 }
 
 const useObjectUrl = (): UseObjectUrlReturn => {
   const [url, setUrl] = useState('');
 
-  const clear = () => {
-    setUrl((prev) => {
-      if (prev) {
-        URL.revokeObjectURL(prev);
-      }
-      return '';
-    });
-  };
-
   const setFile = (file: File) => {
-    const nextUrl = URL.createObjectURL(file);
-
-    setUrl((prev) => {
-      if (prev) {
-        URL.revokeObjectURL(prev);
-      }
-      return nextUrl;
-    });
+    setUrl(URL.createObjectURL(file));
   };
 
   useEffect(() => {
@@ -37,7 +20,7 @@ const useObjectUrl = (): UseObjectUrlReturn => {
     };
   }, [url]);
 
-  return { url, setFile, clear };
+  return { url, setFile };
 };
 
 export default useObjectUrl;
