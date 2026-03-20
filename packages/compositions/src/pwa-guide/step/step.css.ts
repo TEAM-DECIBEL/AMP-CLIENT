0c6088e8f159dc6f22fd198e05866e5cad1f9aaf
Sohyunnnn <thgus05061@naver.com>
2026-03-20 13:49:47 +0900

    style: 스타일 순서 변경

--- packages/compositions/src/pwa-guide/step/step.css.ts
+++ packages/compositions/src/pwa-guide/step/step.css.ts
@@ -3,12 +3,12 @@ import { style } from '@vanilla-extract/css';
 import { ampThemeVars } from '@amp/ads-ui/styles';
 
 export const container = style({
-  padding: '1.6rem',
-  background: ampThemeVars.color.gray_50,
-  border: `1px solid ${ampThemeVars.color.gray_200}`,
   display: 'flex',
   flexDirection: 'column',
   gap: '1rem',
+  padding: '1.6rem',
+  background: ampThemeVars.color.gray_50,
+  border: `1px solid ${ampThemeVars.color.gray_200}`,
   borderRadius: '20px',
 });
 
@@ -18,18 +18,18 @@ export const title = style({
 });
 
 export const step = style({
-  width: '2.4rem',
-  height: '2.4rem',
-  ...ampThemeVars.font.body_m_16,
-  color: ampThemeVars.color.gray_000,
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
+  width: '2.4rem',
+  height: '2.4rem',
   background: ampThemeVars.color.primary,
   borderRadius: '6px',
+  color: ampThemeVars.color.gray_000,
+  ...ampThemeVars.font.body_m_16,
 });
 
 export const description = style({
-  ...ampThemeVars.font.body_m_16,
   color: ampThemeVars.color.gray_900,
+  ...ampThemeVars.font.body_m_16,
 });
